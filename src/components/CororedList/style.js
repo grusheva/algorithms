2f7a6c7b35fd3046fe9453d0f36f0571ffa1d073
maryna.grusheva <maryna.grusheva@sigma.software>
2021-02-25 15:59:50 +0200

    [optimization] - use div instead MuiButton

--- src/components/CororedList/style.js
+++ src/components/CororedList/style.js
@@ -1,20 +1,18 @@
-import { Box as MuiBox, withStyles } from '@material-ui/core';
+import { styled } from '@material-ui/core';
 import { COLORS } from '../../assets/const';
 
-export const Box = withStyles({
-  root: {
-    '&.container': {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    '&.item': {
-      height: '20px',
-      width: '20px',
-      border: `5px solid ${COLORS.sidebarBg}`,
-      transition: 'all .2s ease-in-out',
-
-      '&.selected': { borderColor: COLORS.red },
-      '&:hover': { transform: 'scale(1.5)' },
-    },
+export const Box = styled('div')({
+  '&.container': {
+    display: 'flex',
+    flexWrap: 'wrap',
   },
-})(MuiBox);
+  '&.item': {
+    height: '20px',
+    width: '20px',
+    border: `5px solid ${COLORS.sidebarBg}`,
+    transition: 'all .2s ease-in-out',
+
+    '&.selected': { borderColor: COLORS.red },
+    '&:hover': { transform: 'scale(1.5)' },
+  },
+});
