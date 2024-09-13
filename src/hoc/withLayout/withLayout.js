850f4acda30d4c30545bd296745c1c883be4bb6c
maryna.grusheva <31867554+grusheva@users.noreply.github.com>
2024-09-13 21:59:10 +0300

    disable lint

--- src/hoc/withLayout/withLayout.js
+++ src/hoc/withLayout/withLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
 
+/* eslint-disable react/display-name */
 export const withLayout = Component => props => (
   <Layout>
     <Component {...props} />
