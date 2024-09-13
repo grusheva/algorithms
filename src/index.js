aaf067c02ded88f166051fb205d634c5c7c3015a
maryna.grusheva <31867554+grusheva@users.noreply.github.com>
2024-09-13 21:50:40 +0300

    fix package, names

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { ConnectedRouter } from 'connected-react-router';
 import { store, history } from './store';
 import { Routes } from './routes/Routes';
 
-import './assets/webfonts/Montserrat/Montserrat.css';
+// import './assets/webfonts/Montserrat/Montserrat.css';
 
 ReactDOM.render(
   <Provider store={store}>
